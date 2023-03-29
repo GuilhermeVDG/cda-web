7347eefd35660e5a9090520816ab91edf3c58021
Guilherme Vanderley <guivdouradog@gmail.com>
2023-03-29 00:05:33 -0300

    init header options

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
  return (
-   <div className='container'>
-      header component
-   </div>
+  <header>
+    <Link className='headerOption' to='/'>Home</Link>
+    <Link className='headerOption' to='/quemsomos'>Quem Somos</Link>
+    <Link className='headerOption' to='/areasdeatuacao'>Áreas de Atução</Link>
+    <Link className='headerOption' to='/resultados'>Resultados</Link>
+  </header>
  );
 }
